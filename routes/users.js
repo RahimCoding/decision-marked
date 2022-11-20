ac8ce4f08acde45b3eed5d2b623d8ecb429a983a
Tuigun Bekishev <tbekishev@gmail.com>
2022-11-20 10:35:59 -0400

    add mailgun change mailgun

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,26 @@ const router  = express.Router();
 
 router.get('/', (req, res) => {
   res.render('users');
-}); 
+});
 
+router.post('/poll', (req, res) => {
+  console.log(req.body)
+  const api_key = '';
+  const domain = 'sandbox2d3ff9f87e114f8692c8bc1a7e404057.mailgun.org';
+  const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+  const data = {
+    from: req.body.email,
+    to: req.body.emails,
+    subject: 'Testing Decision Maker',
+    text: `${req.body.question}`
+  };
+
+  mailgun.messages().send(data, function (error, body) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(body);
+  });
+})
 module.exports = router;
