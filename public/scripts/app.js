625c20f3f9d0e0c80440d9c8a9d1256504c152d8
Rahim Jamal <rahim_92@live.ca>
2022-11-20 18:56:24 -0500

    add more options

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,22 @@
 // Client facing scripts here
+$(document).ready(function () {
+  let count = 2;
+  const buttonOption = function () {
+    count++;
+    let $optionString = $(`<input class="form-control">`)
+      .attr('id', 'exampleFormControlInput' + count)
+      .attr('name', 'option' + count)
+      .attr('type', 'text')
+      .attr('placeholder' ,'option ' + count);
+    $("#options").append($optionString);
+  };
+  $("#button").click(function (event) {
+    event.stopPropagation();
+    buttonOption();
+  });
+
+}
+);
+
+
+
