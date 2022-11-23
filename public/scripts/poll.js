7cbbb84a133743130c7e0aa5b14012238ba68b42
Tuigun Bekishev <tbekishev@gmail.com>
2022-11-23 13:45:06 -0400

    add voting with email only

--- public/scripts/poll.js
+++ public/scripts/poll.js
@@ -13,30 +13,32 @@ $(document).ready(function () {
       data.poll.reverse().forEach(element => {
         $('#sort').append(`<li id = ${element.option}>${element.option}</li>`);
       });
-      $('.poll').append(`<input name="email" type="email" class="form-control" id="vote-email" placeholder="name@example.com">`);
+      $('.poll').append(`<input name="email" type="email" id="vote-email" placeholder="name@example.com"><br>`);
       $('.poll').append(`<button id = "choice" type = "button">Submit your results!</button>`);
       $('#sort').sortable();
 
       $("#choice").on('click', () => {
         const voteEmail = $('#vote-email').val();
-       if (poll.sent_email.includes(voteEmail) || poll.email.includes(voteEmail)) {
-
+       if ((poll.email.includes(voteEmail) || poll.sent_email.includes(voteEmail)) && voteEmail) {
          const s = $('#sort').sortable('toArray');
          const sReverse = s.reverse();
          const obj = {}
          sReverse.forEach((element, index) => { //send to the ranking row in the polls table
-          obj[element] = index;
-        });
-        console.log(sReverse);
-        console.log("poll[o]:",poll.url);
-        console.log("obj:",obj);
-        $.ajax({
-          method: "PUT",
-          url: "/users" + window.location.pathname,
-          data: obj
-        });
-        window.location.href = `/poll/${poll.url}/result`;
-      }
+           obj[element] = index;
+         });
+         console.log(sReverse);
+         console.log("poll[o]:",poll.url);
+         console.log("obj:",obj);
+         $.ajax({
+           method: "PUT",
+           url: "/users" + window.location.pathname,
+           data: obj
+         });
+         window.location.href = `/poll/${poll.url}/result`;
+        } else {
+        console.log(voteEmail)
+        alert('You can not vote this poll. Enter correct email')
+        }
 
       });
 
