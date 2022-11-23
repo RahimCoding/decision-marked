bd1710eaa2c22f3dc3f03044fc40558962e7bc76
Tuigun Bekishev <tbekishev@gmail.com>
2022-11-23 18:58:31 -0400

    add question on a result page

--- public/scripts/result.js
+++ public/scripts/result.js
@@ -14,11 +14,12 @@ $(document).ready(function() {
       if (sum === 0) {
         $('body').append('<h1>Thank you! Your poll is created.</h1>');
         $('#chart').hide();
-      }
-      let options = [];
-      let rankings = [];
-      for (const element of poll) {
-        options.push(element.option);
+      } else {
+        $(`<h2>${poll[0].question}</h2>`).insertBefore('#chart');
+        let options = [];
+        let rankings = [];
+        for (const element of poll) {
+          options.push(element.option);
         rankings.push(element.ranking);
       }
       const ctx = $('#chart');
@@ -40,6 +41,7 @@ $(document).ready(function() {
           }
         }
       });
+    }
     });
   };
 
